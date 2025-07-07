73c052f174df39d2b49bbb50fce38d8496e3dd1b
fatihyksell <yukselahmetfatihh@gmail.com>
2025-07-07 10:27:11 +0300

    Initial Commit

---
+++ UIKitProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitProgrammaticUI
+//
+//  Created by Fatih Yuksel on 7.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
